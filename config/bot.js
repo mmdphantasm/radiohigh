c5c00e282670344e7b26558e391dbf40c09c3cda
mmdphantasm <76917761+mmdphantasm@users.noreply.github.com>
2021-03-02 13:37:51 +0330

    radiohigh

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'Nzk0MTk4MjA4MTU0ODI4ODUw.X-3UuA.-xxD7RfR0qoSCIyAHkwTlYOGn1Q',
+        prefix: '!',
+        activity: '!help',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
